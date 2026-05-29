760bb2501c749d7e5a41789f494d0aa027e19750
Striver Zack <monirbot621@gmail.com>
2026-05-30 00:33:07 +0600

    Add aliases and update message format in boss.js

--- Script/boss.js
+++ Script/boss.js
@@ -1,10 +1,13 @@
 // scripts/boss.js
 
 module.exports = {
-name: "boss",
+  name: "boss",
+  aliases: ["owner", "admin"],
 
-run({ api, event }) {
-api.sendMessage(
+  run({ api, event }) {
+
+    api.sendMessage(
+
 `╭━━━〔 👑 𝘽𝙤𝙨𝙨 𝙄𝙣𝙛𝙤 👑 〕━━━⬣
 
 🌸 Name : Striver Zack (Onto) 🙂✨
@@ -19,11 +22,13 @@ api.sendMessage(
 🤲 সবাই আমার boss এর জন্য
 দোয়া করবেন 💖
 
-🔗 Boss :
+🔗 Facebook :
 https://www.facebook.com/profile.php?id=61572053975146
 
 ╰━━━━━━━━━━━━━━⬣`,
-event.threadID
-);
-}
+
+      event.threadID
+    );
+
+  }
 };
